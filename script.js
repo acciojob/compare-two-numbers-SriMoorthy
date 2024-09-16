bb61ed17def39210e2150226f8e7bdda28bbab2d
SriMoorthy <srimoorthy27@gmail.com>
2024-09-16 14:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 let n1 = prompt('Enter the Number:')
 let n2 = prompt('Enter the Number:')
 
-if(n1==n2) return alert('true')
-else return alert('false')
+if(n1==n2){
+	alert('true')
+}
+else {
+	alert('false')
+}
 
